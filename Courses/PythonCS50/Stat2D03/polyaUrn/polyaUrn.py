af51169404ef617b6edd104aa236f308e23e5855
swagbit1 <gagan.bhattarai25@gmail.com>
2025-11-08 12:55:37 -0500

    quick update

--- Courses/PythonCS50/Stat2D03/polyaUrn/polyaUrn.py
+++ Courses/PythonCS50/Stat2D03/polyaUrn/polyaUrn.py
@@ -1,6 +1,6 @@
 import random 
 
-steps = 200000000
+steps = 10
 urn = [1,2]
 
 for i in range(steps):
@@ -13,4 +13,16 @@ for i in range(steps):
 
 print(urn[:5])
 print(f"1 Count: {urn.count(1)},  {(urn.count(1)/len(urn)) * 100:.2f}%\n2 Count: {urn.count(2)},   {(urn.count(2)/len(urn)) * 100:.2f}%")
+
+
+
+def fac(n):
+    if n == 0:
+        return 1
     
+    if n == 1:
+        return 1
+    
+    return n * fac(n - 1)
+
+print(f"Prob of such sequence: {((fac(urn.count(1)) * fac(urn.count(2)))/fac(len(urn))) * 100:.2f}%")
