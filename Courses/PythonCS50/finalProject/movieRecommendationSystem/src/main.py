6cfb74a5fc523e6405f778e075cd8ede5a3dd78b
swagbit1 <gagan.bhattarai25@gmail.com>
2025-12-30 16:01:14 -0500

    quick update

--- Courses/PythonCS50/finalProject/movieRecommendationSystem/src/main.py
+++ Courses/PythonCS50/finalProject/movieRecommendationSystem/src/main.py
@@ -1,5 +1,6 @@
 import read
 import update
+import sys
 
 # helper module files
 
@@ -10,13 +11,57 @@ def main():
     global ratings
 
     #print(read.readFile())
-    print(ratings)
-    ratings.append({
-        "movieId": 3,
-        "rating": 2,
-        "timestamp": "2024-21-21"
-    })
-    update.updateRatings(ratings)
+    #print(ratings)
+    # ratings.append({
+    #     "movieId": 3,
+    #     "rating": 2,
+    #     "timestamp": "2024-21-21"
+    # })
+    # update.updateRatings(ratings)
+
+    menuInterface()
+
+def menuInterface():
+    print("1. View Movie Catalog")
+    print("2. Rate A Movie")
+    print("3. View My Ratings")
+    print("4. Get Recommendations")
+    print("5. Help")
+    print("6. Exit")
+
+    while True:
+        try:
+            choice = int(input("Enter a number: "))
+            break
+        except ValueError:
+            print("Please enter a number from the given table!")
+
+    match choice:
+        case 1:
+            viewCatalog()
+        case 2:
+            rateMovie()
+        case 3:
+            viewRatings()
+        case 4:
+            getRecommendations()
+        case 5:
+            getHelp()
+        case 6:
+            sys.exit("Exited Program")
+
+def viewCatalog():
+    pass
+def rateMovie():
+    pass
+def viewRatings():
+    pass
+def getRecommendations():
+    pass
+def getHelp():
+    pass
+
+
 
 
 
